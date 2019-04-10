b7933a3e3fe418e6e22e93a97248157cae4d28c7
Jean-Pascal Boudet <dev@guitaresphere.com>
2019-04-10 15:03:05 +0200

    exo-4

--- exo-1/script.js
+++ exo-1/script.js
@@ -1,8 +1,9 @@
 // you can write js here
-var kelvin = prompt("Quelle est la température en Kelvin aujourd'hui ?");
-//let kelvin = 294 ;
-let celsius = kelvin - 273; //affectation à celsius la  soustraction 273 à  294 (kelvin)
-let fahrenheit = celsius * (9/5) + 32 ; // multiplier celsius à la division 9/5 puis lui ajouer 32
+var kelvin = parseInt(prompt("Quelle est la température en Kelvin aujourd'hui ?"));
+console.log(typeof kelvin) ;
+//let kelvin = 500 ;
+let celsius = kelvin - 273.15; //affectation à celsius la  soustraction 273 à  294 (kelvin)
+let fahrenheit = (celsius * (9/5)) + 32 ; // multiplier celsius à la division 9/5 puis lui ajouer 32
 
 fahrenheit = Math.floor(fahrenheit); // on affecte à fareiheit l'arrondie de fareiheit
 
--- exo-4/script.js
+++ exo-4/script.js
@@ -1,5 +1,50 @@
 // you can write js here
-
 console.log('exo-4');
 
+function removeFromArr(ElementToRemove, arr){
+
+    for (i = 0; i < arr.length;i++){
+        if (ElementToRemove.indexOf(arr[i]) > -1){
+            arr[i] = "know";
+        }
+    }
+
+return arr;
+}
+
+var elRemove =['get', 'right', 'the', 'first', 'time,'];
 var secretMessage = ["Learning", "isn't", "about", "what", "you", "get", "easily", "the", "first", "time,", "it's", "about", "what", "you", "can", "figure", "out.", "-2015,", "Chris", "Pine,", "Learn", "JavaScript"];
+
+let temp = secretMessage;
+
+console.log(secretMessage);
+
+
+//1
+secretMessage.pop(); //remove from the end
+console.log(secretMessage);
+
+//2
+//add two elements on this array
+temp = secretMessage.push('to', 'program');
+console.log(secretMessage);
+
+
+//3 remplace l'element selectionné par un autre
+secretMessage[secretMessage.indexOf('easily')] = 'right';
+console.log(secretMessage);
+
+//4 supprime le premier element
+secretMessage.shift();
+console.log(secretMessage);
+
+//5 ajoute un element à la premiere place
+secretMessage.unshift('Programming');
+console.log(secretMessage);
+
+//6
+secretMessage = removeFromArr(elRemove,secretMessage);
+console.log(secretMessage);
+
+//7
+console.log(secretMessage.join());
