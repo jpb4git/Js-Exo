2828fdf5ca904321da3f62b1d891de4c95b8978d
jp <infocours@guitaresphere.com>
2019-04-22 22:37:01 +0200

    animation falling Coin

--- exo-10/app.js
+++ exo-10/app.js
@@ -1,3 +1,4 @@
+
 const sourceImg = ['img/empty.svg','img/orange.svg','img/purple.svg','img/empty-ringLess.svg','img/orange-ringLess.svg','img/purple-ringLess.svg'];
 const EMPTY_CELL        = 0;
 const ORANGE_CELL       = 1;
@@ -6,47 +7,98 @@ const EMPTY_RINGLESS    = 3;
 const ORANGE_RINGLESS   = 4;
 const PURPLE_RINGLESS   = 5;
 
+const HumaniseColor = { 4  : 'player Orange',5  : 'player Purple' };
+const cssColor = { 4  : '#FF4500',5  : '#9400D3' };
+
+//---------------------------
 const WIDTH_GRILLE      = 7;
 const HEIGHT_GRILLE     = 6;
-
-const NO_DROP = false;
-const DROP = true;
-const NO_ACTION =false;
-const ACTION =true;
-
-// set the turn for players first play with orange coin  ... 
+//--------------------------
+const NO_DROP           = false;
+const DROP              = true;
+const NO_ACTION         =false;
+const ACTION            =true;
+// set the turn for players. first play with orange coin  ... 
 let stateColor = ORANGE_RINGLESS;
 
-let grilleAction =[EMPTY_RINGLESS,EMPTY_RINGLESS,EMPTY_RINGLESS,EMPTY_RINGLESS,EMPTY_RINGLESS,EMPTY_RINGLESS,EMPTY_RINGLESS ];
+let grilleAction =[
+                    EMPTY_RINGLESS,
+                    EMPTY_RINGLESS,
+                    EMPTY_RINGLESS,
+                    EMPTY_RINGLESS,
+                    EMPTY_RINGLESS,
+                    EMPTY_RINGLESS,
+                    EMPTY_RINGLESS
+                 ];
 
 let grille = [
-    [EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL],
-    [EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL],
-    [EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL],
-    [EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL],
-    [EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL],
-    [EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL],
-];
+                [EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL],
+                [EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL],
+                [EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL],
+                [EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL],
+                [EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL],
+                [EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL],
+            ];
 
-/**************************************************************************************************** */
+const fallLimitAnim = [
+  () => img.style.transform = "translateY(0, 0px)",
+  () => img.style.transform = "translateY(0, 77px)",
+  () => box.style.transform = "translate(0px, 155px)",
+  () => box.style.transform = "translate(0px, 233px)",
+  () => box.style.transform = "translate(0px, 312px)",
+  () => box.style.transform = "translate(0px, 390px)",
+
+]            
+/*-----------------------------------------------------------------------------------------------------*/
+
+
+
+
+
+/************************************* MAIN PROGRAM *************************************************************** */
 init();
 
 
 
 
 
-/************************************************************************************************ */
-
-
-// UNE FONCTION INIT QUI CREER LA TABLE 
+/******************************************** FUNCTIONS **************************************************** */
 function init(){ 
     tableDraw(WIDTH_GRILLE,1,document.getElementById('header'),grilleAction,NO_ACTION,DROP);
     tableDraw(WIDTH_GRILLE,HEIGHT_GRILLE,document.getElementById('main'),grille,ACTION,NO_DROP);
 }
 
+function cleanDraw(){
+    return  [
+                [EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL],
+                [EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL],
+                [EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL],
+                [EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL],
+                [EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL],
+                [EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL],
+            ];
+}
+
+function winMsg(element){
+   var body = element;
+   body.innerHTML = '';
+   var win = document.createElement("div");
+   win.classList.add('win');
+   var h1 = document.createElement("H1");
+   h1 = document.createElement("H1");
+   h1.style.color = cssColor[stateColor];
+   var t = document.createTextNode(HumaniseColor[stateColor] + " Win this game." );
+   var img = document.createElement("img");
+   img.src = setImgSource(stateColor);
+   h1.appendChild(t);
+   win.appendChild(h1);
+   win.appendChild(img);
+   body.appendChild(win);
+   console.log(HumaniseColor[stateColor] + " Win this game.");
+}
+
 function tableDraw(width,height,element,grille,Action,Drop) {
-    //body reference 
-    
+    //body reference   
     var main = element;
     main.innerHTML = '';
     // create elements <table> and a <tbody>
@@ -81,8 +133,8 @@ function tableDraw(width,height,element,grille,Action,Drop) {
         
 
 
-        cell.appendChild(img);
-        row.appendChild(cell);
+      cell.appendChild(img);
+      row.appendChild(cell);
       }
       //row added to end of table body
       tblBody.appendChild(row);
@@ -93,44 +145,97 @@ function tableDraw(width,height,element,grille,Action,Drop) {
     main.appendChild(tbl);
   
   }
-
-/**
- * 
- * @param {*} indexSource 
- * @return String   //source path
- */
-  function setImgSource(indexSource){   
+function setImgSource(indexSource){   
     return  sourceImg[indexSource];    
   }
 
-  function dropCoin(){
-        /// here we drop coin on column 
+function dropCoin(){
+        let indexI = 0;
+        let indxJ  =0;
+        /// here we drop coin on column // checking from bottom . 
         for (i = HEIGHT_GRILLE-1 ; i >= 0; i--){              
             if( grille [i][this.dataset.index] == EMPTY_CELL ){
                 grille[i][this.dataset.index]= stateColor;
-                checkWinState(i,this.dataset.index);
+                indexI = i;
+                indexJ = this.dataset.index;
                 break;
             }    
         }
+        
+        animateCoinInTd(indexJ,indexI);
+        setTimeout(function() {
         //we draw the board 
         tableDraw(WIDTH_GRILLE,HEIGHT_GRILLE,document.getElementById('main'),grille,ACTION,NO_DROP);        
-
-        // we check if player win the game 
-        checkWinState();
+       
+        checkWinState(grille,indexJ,indexI);        
         
+        
+        }, 1000);
         // we change the stateColor
         stateColor = switchStateColor(); 
-     
+
+        //switch visual color player
+        this.src=sourceImg[stateColor]; 
   }
-  function showCoin(){
+function showCoin(){
     this.src=sourceImg[stateColor]; 
 }
+
 function leaveCoin(){
     this.src=sourceImg[EMPTY_RINGLESS]; 
 }
- // une fonction qui remplie  la table avec les img 
+ 
+ /**
+  * 
+  */
+function animateCoinInTd(indexJ,indexI){
+// animation draw
+        var imgAnim = document.createElement('img');
+        imgAnim.src = setImgSource(stateColor);
+        imgAnim.classList.add('grilleNode');
+        
+        imgAnim.classList.add(selectMoveCoinClass(parseInt(indexJ)));
+       
+        imgAnim.style.zIndex=-1;
+       
+        
+        document.getElementById('main').appendChild(imgAnim);
+        var mo = document.getElementsByClassName(selectMoveCoinClass(parseInt(indexJ)));
+      
+        mo[0].style.left = "" + 85 * parseInt(indexJ)  + "px";
+       mo[0].style.animation = "CoinAnimate6 2s";
+}
 
- function switchStateColor(){
+function selectMoveCoinClass(index){
+    
+    switch (index) {
+        case 0:
+            return "moveCoin1"        
+            break;
+        case 1:
+            return "moveCoin2"        
+            break;
+        case 2:
+            return "moveCoin3"        
+            break;
+        case 3:
+            return "moveCoin4"        
+            break;
+        case 4:
+            return "moveCoin5"        
+            break;
+        case 5:
+            return "moveCoin6"        
+            break;
+
+
+        default:
+            break;
+    }
+}
+
+
+function switchStateColor(){
      if (stateColor == ORANGE_RINGLESS) {
         return PURPLE_RINGLESS;
      }else{
@@ -138,12 +243,64 @@ function leaveCoin(){
      }
 
  }
-/**
- * 
- */
- function checkWinState(col,row){
 
+function checkWinState(grille,col,row){
+       // alert(col + ' - ' + row)
+        resultstack = horizontalCheck(grille,col,row)
+        if (resultstack){
+              winMsg(document.body);
 
+        }else{  
+
+        }
  }
+
+function horizontalCheck(grille,col,row){
+   
+    let stackcolor =0;
+    let indexFirst = 0;
+    col = parseInt(col);
+    row = parseInt(row);
+     
+    for(x = 0 ; x < WIDTH_GRILLE;x++){
+           if (grille[row][x] == stateColor){
+                    indexFirst = x;
+                    break; 
+           }
+    }  
+    for (i = x ; i < WIDTH_GRILLE;i++){
+                if (grille[row][i] == stateColor){
+                    stackcolor++;
+                }else{
+                    break;
+                }
+    }
+
+    return (stackcolor == 4) ? true : false;         
+ }
+function verticalCheck(grille,col,row){
+    stateColor   //player
+    let stackcolor =0;
+    let indexFirst = 0;
+    col = parseInt(col);
+    row = parseInt(row);   
+
+    for(x = 0 ; x < HEIGHT_GRILLE;x++){
+           if (grille[X][col] == stateColor){
+                    indexFirst = x;
+                    break; 
+           }
+    }  
+    for (i = x ; i < HEIGHT_GRILLE;i++){
+                if (grille[i][col] == stateColor){
+                    stackcolor++;
+                }else{
+                    break;
+                }
+    }
+return (stackcolor == 4) ? true : false;  
+ }
+function diagonalUpCheck(){}
+function diagonalDownCheck(){}
 
 
